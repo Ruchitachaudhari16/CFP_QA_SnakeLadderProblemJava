e058c6bffc8544234585bec8a865ed1ba67acaf6
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2023-12-31 12:10:46 +0530

    Game start with a single player with start position

---
+++ src/Simulation.java
@@ -0,0 +1,15 @@
+public class Simulation {
+    //Uc1:-Snake and Ladder game played with single player at start position 0
+
+    public void StartPosition()
+    {
+        int startPosition = 0;
+        System.out.println("Game played with single player at StartPosition:"+startPosition);
+    }
+    public static void main(String[] args)
+    {
+        Simulation sm=new Simulation();
+        sm.StartPosition();
+    }
+}
+
