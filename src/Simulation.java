5b1f7e493bd17b8c4077d1e4c5b22cb8794bd1ce
Ruchitachaudhari16 <ruchitachaudhari@gmail.com>
2023-12-31 12:52:49 +0530

    Dice Rolled between 1 to 6

--- src/Simulation.java
+++ src/Simulation.java
@@ -1,15 +1,28 @@
-public class Simulation {
-    //Uc1:-Snake and Ladder game played with single player at start position 0
+import java.util.Random;
 
-    public void StartPosition()
+public class Simulation {
+    //Uc2:-The Player rolls the die to get a number between 1 to 6. - Use ((RANDOM)) to get the number between
+    //1 to 6
+
+    public void StartPosition() //startPosition fun
     {
         int startPosition = 0;
         System.out.println("Game played with single player at StartPosition:"+startPosition);
     }
+    public void RolledDie()
+    {
+        //Random function for 1 to 6
+        Random random=new Random();
+        int x=random.nextInt(1,6);
+        System.out.println("The dice rolled :"+x);
+    }
+
     public static void main(String[] args)
     {
         Simulation sm=new Simulation();
         sm.StartPosition();
+        sm.RolledDie();
+
     }
 }
 
